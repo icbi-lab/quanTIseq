2a6b3fdfdd48ee28d80303458b4fc2cee839b22e
chripla <33951891+chripla@users.noreply.github.com>
2019-02-26 16:24:41 +0100

    Delete dependencies.R

--- dependencies.R
+++
@@ -1,7 +0,0 @@
-chooseCRANmirror(false,1)
-install.packages("MASS") 
-install.packages("limSolve")
-install.packages("tools")
-
-source("https://bioconductor.org/biocLite.R")
-biocLite("preprocessCore")
