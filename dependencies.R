b1c2ed3267b6133c97d29d6af37e2cd4ce421210
Gregor Sturm <mail@gregor-sturm.de>
2020-01-07 15:30:17 +0100

    Remove package 'tools' from R deps

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,6 @@
 chooseCRANmirror(false,1)
 install.packages("MASS") #version >= 7.3-47
 install.packages("limSolve") #version >= 1.5.5.3
-install.packages("tools") #version >= 3.4.3
 
 source("https://bioconductor.org/biocLite.R") #version >= 3.4
 biocLite("preprocessCore") #version >= 1.44.0
